f6c892f3e732428442b0e345386f926d00b9e847
Lee JaeHyun <myleje@gmail.com>
2018-10-16 09:00:11 +0900

    Add files via upload

---
+++ GearVRControllerEmulator.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class GearVRControllerEmulator : MonoBehaviour {
+    public Transform controller;
+    public float hDamping = 2.0f;
+    public float vDamping = 2.0f;
+
+    private float xAngle, yAngle;
+
+	void Start () {
+        Invoke("InitController", 0.5f);
+	}
+
+    void InitController()
+    {
+        if (controller != null)
+        {
+            controller.Find("GearVrControllerModel").gameObject.SetActive(true);
+        }
+    }
+    void Update()
+    {
+        xAngle += Input.GetAxis("Mouse Y");
+        yAngle += Input.GetAxis("Mouse X");
+        controller.localEulerAngles = new Vector3(  xAngle * hDamping
+                                                  , yAngle * vDamping
+                                                  , 0f);
+    }
+}
